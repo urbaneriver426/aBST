916805d840a03f4ea8ba6b62c0a83af6e3f46c55
urbaneriver426 <45755209+urbaneriver426@users.noreply.github.com>
2019-11-08 22:43:09 +0300

    Update aBST.py

--- aBST.py
+++ aBST.py
@@ -1,35 +1,29 @@
 class aBST:
 
-        def __init__(self, depth):
-                self.tree_size = 2 ** (depth+1) - 1
-                self.Tree = [None] * self.tree_size
-	
-        def FindKeyIndex(self, key):
-                i = 0
-                while True:
-                        if i > self.tree_size - 1 or self.Tree[i] is None:
-                                return None
-                        else:
-                                if self.Tree[i] == key:
-                                        return i
-                                elif self.Tree[i] > key:
-                                        i = 2*i + 1
-                                else:
-                                        i = 2*i + 2
+    def __init__(self, depth):
+        self.tree_size = 2 ** (depth+1) - 1
+        self.Tree = [None] * self.tree_size
 
-        def AddKey(self, key):
-                if self.FindKeyIndex(key) is not None:
-                        return self.FindKeyIndex(key)
+    def FindKeyIndex(self, key):
+        i = 0
+        while True:
+            if i > self.tree_size - 1:
+                return None
+            if self.Tree[i] is None:
+            	return -i
+            else:
+                if self.Tree[i] == key:
+                        return i
+                elif self.Tree[i] > key:
+                        i = 2*i + 1
                 else:
-                        i = 0
-                        while True:
-                                if i > self.tree_size - 1:
-                                        return -1
-                                else:
-                                        if self.Tree[i] is None:
-                                                self.Tree[i] = key
-                                                return i
-                                        elif self.Tree[i] > key:
-                                                i = 2*i + 1
-                                        else:
-                                                i = 2*i + 2
+                        i = 2*i + 2
+
+    def AddKey(self, key):
+        if self.FindKeyIndex(key) is not None:
+            if self.FindKeyIndex(key) < 0:
+            	return self.FindKeyIndex(key)
+            else:
+            	self.Tree[self.FindKeyIndex(key)] = key
+        else:
+            return None
